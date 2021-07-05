94bd9c438af89c3bf751f31531750cd2b7296f93
stdtom <stdtom@gmx.net>
2021-07-05 22:36:25 +0200

    Add color codes; change format of success() and err()

--- script.sh
+++ script.sh
@@ -21,6 +21,13 @@ args=()
 # }}}
 # Helpers {{{
 
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[1;31m' GREEN='\033[1;32m' ORANGE='\033[0;33m' BLUE='\033[1;34m' PURPLE='\033[1;35m' CYAN='\033[1;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
 out() {
   ((quiet)) && return
 
@@ -35,8 +42,9 @@ out() {
   printf '%b\n' "$message";
 }
 die() { out "$@"; exit 1; } >&2
-err() { out " \033[1;31m✖\033[0m  $@"; } >&2
-success() { out " \033[1;32m✔\033[0m  $@"; }
+err() { out " ${RED}✖  $@${NOFORMAT}"; } >&2
+success() { out " ${GREEN}✔  $@${NOFORMAT}"; }
+
 
 # Verbose logging
 log() { (($verbose)) && out "$@"; }
@@ -183,6 +191,7 @@ safe_exit() {
 
 # }}}
 # Main loop {{{
+setup_colors
 
 # Print help if no arguments were passed.
 [[ $# -eq 0 ]] && set -- "--help"
